2b57812cceadf49ba6acc77c32d5c427072395de
amy <fangtee@qq.com>
2014-05-18 14:32:14 +0800

    deploy

--- App/View/theme/neat/dashboard.php
+++ App/View/theme/neat/dashboard.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<title>Neat Admin Template</title>
+<title>PtPHP</title>
 <meta name="description" content="">
 <meta name="viewport" content="width=device-width">
 <link rel="stylesheet" href="/static/theme/neat/css/bootstrap.css">
